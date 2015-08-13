8243a7203a94a0339a72db77fdd26f59bda14c3d
Meghan McGarry <mbmcgarry@wisc.edu>
2015-08-13 13:49:30 -0700

    query inventory, calculate emissions, make contour movie, for fake data. Need to apply to real data

--- Kr85_emissions.py
+++ Kr85_emissions.py
@@ -4,6 +4,7 @@
 import cymetric as cym
 import pandas as pd
 import numpy as np
+import math
 from cymetric import root_metric
 
 
@@ -35,19 +36,122 @@ def FacilityInput(dbname):
 
 
 
+def calc_emissions(db):
+ 
+    velocity = 0.6 # m/s
+    rows = 3
+    cols = rows
+    src = [0,1]   # [row,col]
+    n_times = 6
+    curr_emiss = abs(np.ndarray(n_times)*0)
+    curr_emiss[0] = 100
+    curr_emiss[2] = 50
+    curr_emiss[3] = 50
+    
+    pos_matrix = abs(np.ndarray((rows,cols))*0)
+    pos_matrix[src[0]][src[1]] = 1
+    
+    inventory_frame = FacilityInput(dbfile)
+    #n_times = inventory_frame['TimeCreated'].size
+
+    emissions = abs(np.ndarray((n_times,rows,cols))*0)
+    
+    for r in range(rows):
+        for c in range(cols):
+            dist = math.sqrt((r - src[0])**2 + (c - src[1])**2)
+            pos_matrix[r][c] = dist
+    fall_off_matrix = 1/(4*math.pi*(1+pos_matrix)**2)
+    time_delay_matrix = pos_matrix/velocity
+
+    for t in range(n_times):
+        if curr_emiss[t] !=0 :
+            eff_emiss = curr_emiss[t]*fall_off_matrix
+            for r in range(rows):
+                for c in range(cols):
+                    eff_time_delay = time_delay_matrix[r][c]
+                    if (t + eff_time_delay) <= n_times:  # change for real program
+                        emissions[t+eff_time_delay][r][c] += eff_emiss[r][c]
+    return emissions
+
+
+
+
+import matplotlib
+import numpy as np
+import matplotlib.cm as cm
+import matplotlib.mlab as mlab
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+matplotlib.rcParams['xtick.direction'] = 'out'
+matplotlib.rcParams['ytick.direction'] = 'out'
 
 dir = '/Users/mbmcgarry/git/data_analysis/data/testing/random_sep/'
 dbfile = dir+'norm_dist.sqlite'
 
-velocity = 3.0 # m/s
-size = 4
-source = [0,1]
+emissions = calc_emissions(dbfile)
 
-pos_matrix = np.ndarray((size,size))*0
-pos_matrix[source[0]][source[1]] = 1
+#time = 0
+n_times = emissions.shape[0]
+xmin = 0
+ymin = 0
+xmax = emissions.shape[1]
+ymax = emissions.shape[2]
+edge = 0.5
+x = np.arange(xmax)
+y = np.arange(ymax)
+X, Y = np.meshgrid(x, y)
+#Z = emissions[time][:][:]
 
-inventory_frame = FacilityInput(dbfile)
-n_times = inventory_frame['TimeCreated'].size
 
-emissions = np.ndarray((n_times,size,size))*0
+# Create a simple contour plot with labels using default colors.  The
+# inline argument to clabel will control whether the labels are draw
+# over the line segments of the contour, removing the lines beneath
+# the label
+fig = plt.figure()
+plt.xlabel('x location')
+plt.ylabel('y location')
+min_em = np.amin(emissions, axis=2).min()
+max_em = np.amax(emissions, axis=2).max()
+n_lev = (max_em - min_em)/25
+
+#levels = np.arange(-1.2, 1.6, 0.2)
+levels = np.arange(min_em, max_em, n_lev)
+
+def animate(i):
+    Z = emissions[i][:][:]
+    cont = plt.imshow(Z, interpolation='bilinear', origin='lower',
+                extent=(xmin-edge,xmax+edge,ymin-edge,ymax+edge))
+    plt.title("Kr85 emissions, t="+ str(i))
+    # We can still add a colorbar for the image, too.
+    return cont
+
+anim=1
+if anim == 1:
+    anim = animation.FuncAnimation(fig, animate, frames=n_times, repeat_delay=2000, interval=500, blit=False) 
+else:
+   cont = plt.imshow(emissions[5][:][:], interpolation='bilinear', origin='lower',
+                extent=(xmin-edge,xmax+edge,ymin-edge,ymax+edge))
+   CB = plt.colorbar(cont, shrink=0.8, extend='both')
+
+
+
+#CBI = plt.colorbar(anim, shrink=0.8)
+ 
+
+#plt.figure()
+#im = plt.imshow(Z, interpolation='bilinear', origin='lower',
+#                extent=(xmin-edge,xmax+edge,ymin-edge,ymax+edge))
+#CS = plt.contour(Z, levels,
+#                 origin='lower',
+#                 linewidths=2,
+#                 extent=(xmin,xmax,ymin,ymax))
+
+
+
+# make a colorbar for the contour lines
+#CB = plt.colorbar(CS, shrink=0.8, extend='both')
+
+plt.show()
+
 
