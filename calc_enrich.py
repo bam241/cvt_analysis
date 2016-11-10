0897d2471bf23f682128a8d6a74c2866bf22267f
Meghan McGarry <mbmcgarry@wisc.edu>
2016-11-10 15:20:30 -0600

    most but not equations have been checked. Still a problem with number machines in a stage

--- calc_enrich.py
+++ calc_enrich.py
@@ -14,49 +14,35 @@ cut = 0.50
 k = 2.0  # L/F ratio
 
 # Centrifuge parameters
-v_a = 485.0 # m/s
-Z = 1.0   # m
-d = 0.15  # m 
-F_flow = 15e-6 # kg/s (paper is in mg/s)
+#v_a = 485.0 # m/s
+#Z = 1.0   # m
+#d = 0.15  # m 
+#F_m = 15e-6 # kg/s (paper is in mg/s)
 
-a = d/2.0 # outer radius
-r_2 = 0.99*a  # fraction of a
 
-def calc_del_U():
+def calc_del_U(v_a, Z, d, F_m):
+    a = d/2.0 # outer radius
+    r_2 = 0.99*a  # fraction of a
     
-    # Centrifuge assumptions
-    #    Z = 2 # m
-    #    r2 = 0.96  # Range 0.96 - 0.99 (Glaser)
-    #    T = 273 + 70 # Celsius to kelvin 
-    #    r_a = 0.05 #m
-    #    omega = 63000/60 #rpm -> rot/sec
-    #v_a = omega*r_a
-
-    L_flow = k*F_flow
 
     # Intermediate calculations
     r_12 = np.sqrt(1.0 - (2.0*R*T*(np.log(x))/M/(v_a**2))) # fraction
     r_1 = r_2*r_12  # fraction
-    
-    L_F = L_flow/F_flow  #range 2-4
+
+    # Glaser eqn 12
+    L_F = k  #range 2-4
     Z_p = Z*(1.0 - cut)*(1.0 + L_F)/(1.0 - cut + L_F)
 
     print "L_F= ", L_F
     print "Z_p=  ", Z_p
-    
+
+    # Glaser eqn 3
     C1 = (2.0*np.pi*D_rho/(np.log(r_2/r_1)))
-    A_p = C1 *(1.0/F_flow) * (cut/((1.0 + L_F)*(1.0 - cut + L_F)))
-    A_w = C1 * (1.0/F_flow) * ((1.0 - cut)/(L_F*(1.0 - cut + L_F)))
+    A_p = C1 *(1.0/F_m) * (cut/((1.0 + L_F)*(1.0 - cut + L_F)))
+    A_w = C1 * (1.0/F_m) * ((1.0 - cut)/(L_F*(1.0 - cut + L_F)))
 
-    print "C1= ", C1
-    print "A_p=  ", A_p
-    print "A_w=  ", A_w
-
-    
-    C_flow = 0.5*F_flow*cut*(1.0 - cut)
-    C_therm = (dM * (v_a**2))/(2.0 * R * T)
-    print "C_flow=  ", C_flow
-    print "C_therm=  ", C_therm
+    C_flow = 0.5*F_m*cut*(1.0 - cut)
+    C_therm = calc_C_therm(v_a)
 
     C_scale = ((r_2/a)**4)*((1-(r_12**2))**2)
     bracket1 = (1 + L_F)/cut
@@ -64,80 +50,137 @@ def calc_del_U():
     bracket2 = L_F/(1 - cut)
     exp2 = np.exp(-1.0*A_w*(Z - Z_p))
 
-    print "C_scale=  ", C_scale
-    print "bracket1= ", bracket1
-    print "bracket2= ", bracket2
-    print "exp1= ", np.exp(exp1)
-    print "exp2= ", np.exp(exp2)
-
+    # Glaser eqn 10
+    major_term = 0.5*cut*(1.0 - cut)*(C_therm**2)*C_scale*(
+                (bracket1*(1 - exp1)) + (bracket2*(1 - exp2)))**2 # kg/s    
+    del_U = F_m*major_term #kg/s
     
-    del_U = 0.5*F_flow*cut*(1.0 - cut)*(C_therm**2)*C_scale*(
-        (bracket1*(1 - exp1)) + (bracket2*(1 - exp2)))**2 # kg/s
-        
     per_sec2yr = 60*60*24*365.25 # s/m * m/hr * hr/d * d/y
 
+    # Glaser eqn 6
     dirac = 0.5*np.pi*Z*D_rho*C_therm*per_sec2yr  # kg/s
-    del_U = del_U * per_sec2yr
+    del_U_yr = del_U * per_sec2yr
 
-    alpha = np.exp(np.sqrt(2)*C_therm*Z/d)
+    # Avery p.18
+    # del_U in moles/sec
+    del_U_moles = del_U/M
+    alpha = alpha_by_swu(del_U, F_m)
+    #1 + np.sqrt((2*del_U_moles*(1-cut)/(cut*F_m)))
     
-    return alpha, del_U, dirac
+    return alpha, del_U, del_U_yr
 
+# for a machine
+def calc_C_therm(v_a):
+    C_therm = (dM * (v_a**2))/(2.0 * R * T)
+    return C_therm
 
 def calc_V(N_in):
     V_out = (2.0*N_in - 1.0)*np.log(N_in/(1.0 - N_in))
     return V_out
+
+# for a machine
+def alpha_by_swu(del_U, F_m):
+    # Avery p.18
+    # del_U in moles/sec
+    del_U_moles = del_U/M
+    alpha = 1 + np.sqrt((2*del_U_moles*(1-cut)/(cut*F_m)))
+    return alpha
+
+# for a machine
+def alpha_by_enrich(Nf, Np):
+    num = Np/(1 - Np)
+    denom = Nf/(1-Nf)
+    alpha_enr = num/denom
+    return alpha_enr
+
+# for a machine
+def alpha_max_theory(v_a, Z, d):
+    # Avery p36
+    # Max theoretical separation for zero withdrawal
+    C_therm = calc_C_therm(v_a)
+    alpha_th = np.exp(np.sqrt(2)*C_therm*Z/d)
+
+# for a machine
+def product_by_alpha(alpha, Nfm):
+    ratio = (1.0 - Nfm)/(alpha*Nfm)
+    Npm = 1.0/(ratio + 1.0)
+    return Npm
+
     
-# **** NOT WORKING ***
-def calc_alpha():
-    # define Feed, del_U from above
-    # assume one of Np, Nf, Nw
+def stages_per_cascade(alpha=1.0417, Nfc=0.007, Npc=0.03, Nwc=0.002):
+    epsilon = alpha - 1.0
+    enrich_inner = (Npc/(1.0 - Npc))*((1.0 - Nfc)/Nfc)
+    strip_inner =  (Nfc/(1.0 - Nfc))*((1.0 - Nwc)/Nwc)
 
-    # Define NF
-    alpha = 1.0 + ((2.0*cut -1)/(Nf - 0.5))
-    Np = 1.0/(1+ ((1-Nf)/(alpha*Nf)))
-    Nw = (Nf - cut*Np)/(1.0 - cut)
-
-    Vf = calc_V(Nf)
-    Vp = calc_V(Np)
-    Vw = calc_V(Nw)
-
-    # Create a table of Np, Vp
-    n_steps = 10
-    V = []
-    for x in range(0, n_steps):
-        real_x = x/n_steps
-        value = (2.0*x - 1)*np.log(x/(1.0 - x))
-        V.append(value)
-
-    
-#    del_U = F*((cut*Vp) + ((1 - cut)* Vw) - Vf)
-
-
-# *** UNTESTED ****
-def n_stages(alpha=1.04, Nf=0.007, Np=0.20, Nw=0.002):
-    enrich_inner = (Np/(1.0 - Np))*((1.0 - Nf)/Nf)
-    enrich_stages = (1.0/(alpha-1.0))*np.log(enrich_inner)
-    strip_inner =  (Nf/(1.0 - Nf))*((1.0 - Nw)/Nw)
-    strip_stages = (1.0/(alpha-1.0))*np.log(strip_inner)
+    enrich_stages = (1.0/epsilon)*np.log(enrich_inner)
+    strip_stages = (1.0/epsilon)*np.log(strip_inner)
 
     return enrich_stages, strip_stages
 
 # *** UNTESTED ****
-def cf_per_stage(Npc, Nwc, Nfs, Pc, alpha):
+# CAN ANY OF THIS BE DEFINED WITH CUT???
+def machines_per_enr_stage(alpha, del_U, Nfs, Npc, Pc):
+    # flows do not have required units so long as they are consistent
     # Npc, Nwc, Nfc = enrichment of cascade product/waste/feed
     # Nfs, Nws, Nps = enrichment of stage product/waste/feed
     # Pc = cascade product flow
     
-    epsilon = 1.0 - alpha
-    # L_stage = counter-current flow of stage
-    L_stage_enrich = 2*Pc*(Npc - Nfs)/(epsilon*Nfs*(1-Nfs)) # Check book?
-    L_stage_strip = 2*Wc*(Nfs - Nwc)/(epsilon*Nfs*(1-Nfs)) # Check book?
+    epsilon = alpha - 1.0
 
+    # F_stage = incoming flow (in Avery denoted with L_r)
+    # Avery p. 60
+    F_stage_enrich = 2*Pc*(Npc - Nfs)/(epsilon*Nfs*(1 - Nfs))
+
+    # Feed flow of a single machine (in Avery denoted with L)
+    # Avery p. 62
+    F_machine = 2.0*del_U/(epsilon**2)
+
+    n_enrich = F_stage_enrich/F_machine
+
+    return n_enrich, F_stage_enrich
+
+def machines_per_strip_stage(alpha, Nfs, Npc, Nwc, Pc, Fc):
+    # flows do not have required units so long as they are consistent
+    # Npc, Nwc, Nfc = enrichment of cascade product/waste/feed
+    # Nfs, Nws, Nps = enrichment of stage product/waste/feed
+    # Pc = cascade product flow
+    
+    epsilon = alpha - 1.0
+    Wc = Fc - Pc
+
+    # F_stage = incoming flow (in Avery denoted with L_r)
+    # Avery p. 60
+    F_stage_strip = 2*Wc*(Nfs - Nwc)/(epsilon*Nfs*(1 - Nfs)) 
+
+    # Feed flow of a single machine (in Avery denoted with L)
+    # Avery p. 62
+    F_machine = 2.0*del_U/(epsilon**2)
+
+    n_strip = F_stage_strip/F_machine
+
+    return  n_strip, F_stage_strip
+
+
+def delta_U_cascade(Npc, Nwc, Fc, Pc):
     Vpc = calc_V(Npc)
     Vwc = calc_V(Nwc)
-    
-    ## WHAT IS EQN FOR TOTAL NUMBER OF STAGES???
-    n_cf = 2*(Pc*Vpc + Wc*Vwc)/(L_stage_tot*(epsilon**2)) # CHECK BOOK
+    Wc = Fc - Pc
+    delta_U_cascade = Pc*Vpc + Wc*Vwc
 
-    return n_cf, L_stage_tot
+    return delta_U_cascade
+
+def machines_per_cascade(del_U_machine, Npc, Nwc, Pc, Wc):
+    # Avery p 62
+    U_cascade = delta_U_cascade(Npc, Nwc, Pc, Wc)
+    n_cf = U_cascade/del_U_machine
+
+    return n_cf
+
+def del_U_by_cascade_config(Npc, Nwc, Pc, Wc, n_cf):
+    U_cascade = delta_U_cascade(Npc, Nwc, Pc, Wc)
+    del_U_machine = U_cascade/n_cf
+
+    return del_U_machine
+
+### NOW USE A PROGRAM TO CALCULARE # stages, number of machines,
+####then del_U per machine
